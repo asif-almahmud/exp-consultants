c6c84611589867d8030d52dc7eab2daa86c15de2
sabujpakhi <asifalmahmud1990@gmail.com>
2022-02-16 16:14:58 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// zULGNpVekoHZky9KgL9lRE5TbaEjWuJbL8uv (SDK Key)
+// LskVaLF3VGUtHn8o96UH64OjQ2vMAijgryks (SDK Secret)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Trying Dyte</div>;
 }
 
 export default App;
